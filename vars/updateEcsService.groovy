2de9ad78df5a36ccc3767cf9ed20d8be6ff71e0f
Ravindra Singh <ravindrasinghh.me@gmail.com>
2023-01-23 23:56:01 +0530

    Update updateEcsService.groovy

--- vars/updateEcsService.groovy
+++ vars/updateEcsService.groovy
@@ -1,6 +1,6 @@
 def call()
 {
     sh '''
-    aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE} --task-definition ${TASKDEFINITION}
+    aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE} --region ${AWSREGION} --task-definition ${TASKDEFINITION}
     '''
 }
