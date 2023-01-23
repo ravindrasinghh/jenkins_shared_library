5d23bf53a4db9874bfd01f797d1fadf6166216b7
Ravindra Singh <ravindrasinghh.me@gmail.com>
2023-01-23 21:28:37 +0530

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,5 +1,5 @@
 def pushDockerImage(dockerImage,dockerTag,ecrRepository) {
-    def loginCommand = "aws ecr get-login-password | docker login --username AWS --password-stdin ${ecrRepository}"
+    def loginCommand = "aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ecrRepository}"
     sh loginCommand
     def tagCommand = "docker tag ${dockerImage}:${dockerTag} ${ecrRepository}:${dockerTag}"
     sh tagCommand
