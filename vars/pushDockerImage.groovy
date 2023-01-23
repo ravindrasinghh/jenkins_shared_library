bed17c9d5b13d39203c1c3865828d0d3581e1d99
Ravindra Singh <ravindrasinghh.me@gmail.com>
2023-01-23 23:34:01 +0530

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -4,6 +4,6 @@ def call()
     aws ecr get-login-password --region ${AWSREGION} | docker login --username AWS --password-stdin ${ECRREPOSITORY}
     docker build  -t ${DOCKERIMAGE}:latest .
     docker tag ${DOCKERIMAGE}:${DOCKERTAG} ${ECRREPOSITORY}/${DOCKERIMAGE}:${DOCKERTAG}
-    docker push ${ECRREPOSITORY}:${DOCKERTAG}
+    docker push ${ECRREPOSITORY}/${DOCKERIMAGE}:${DOCKERTAG}
     '''
 }
