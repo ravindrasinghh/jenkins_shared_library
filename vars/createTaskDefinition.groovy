ef401b48a031e6d9b83be0a11c5e7df2d8bede5e
Ravindra Singh <ravindrasinghh.me@gmail.com>
2023-01-23 23:11:04 +0530

    Update createTaskDefinition.groovy

--- vars/createTaskDefinition.groovy
+++ vars/createTaskDefinition.groovy
@@ -1,5 +1,5 @@
 def call() {
     sh '''
-     "aws ecs register-task-definition --family ${taskDefinition} --cli-input-json --region ${awsRegion} file://${taskDefinitionFile}"
+     "aws ecs register-task-definition --family ${TASK_DEFINITION} --cli-input-json --region ${AWS_REGION} file://${TASK_DEFINITION_FILE}"
     '''
 }
